cd8fe68ae8592826c65dfcdb7d3531abe447e87a
Rickk-mpeg <hcordeirobrito@gmail.com>
2025-06-26 10:43:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 api_key = st.secrets["huggingface"]["api_key"]
 
 def gerar_resposta(prompt):
-    url = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.1"
+    url = "https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta"
     headers = {"Authorization": f"Bearer {api_key}"}
     response = requests.post(url, headers=headers, json={"inputs": prompt})
     if response.status_code == 200:
